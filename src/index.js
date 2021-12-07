f95fa89fe7653e5399d21c2f8142e73340ff59ab
hani <akkoahani@gmail.com>
2021-12-07 12:30:11 +0900

    axios

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app.jsx';
 import Youtube from './service/youtube';
+import axios from 'axios';
 
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: { key: 'AIzaSyCcmMsnLwiSW3nuZFj8E9Q3nc1XXxay764' },
+}); //axios에서 기본적으로 만든 것
+
+const youtube = new Youtube(httpClient);
 
 ReactDOM.render(
   <React.StrictMode>
